7b0e79de72aa434b91351459fb6edda99dbca2f4
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-11-04 20:23:28 +1100

    Add files via upload

---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.65.0"
+    }
+  }
+
+  cloud {
+    organization = "Sharmila"
+    workspaces {
+      name = "azure-storage-deploy-workspace"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
