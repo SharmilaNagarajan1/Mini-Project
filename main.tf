559e74c1e3ee9971691a4615ea4ee4f70f506a6e
sharmila <sharmeeh@gmail.com>
2025-11-06 20:17:31 +1100

    Installed node js  in self hosted runner and added varibles in code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "storageacc" {
   source            = "app.terraform.io/Sharmila/storageacc/azurerm"
   version           = "1.0.0"
-  name_prefix       = "06112025"  
+  name_prefix       =  var.name_prefix
   location          = var.location
-  account_replication_type = "LRS"
+  account_replication_type = var.account_replication_type
 }
